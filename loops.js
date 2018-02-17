b6b742e79ac2d05141e9c4e803434743a647ca23
Jonathan Lin <jlin8293@gmail.com>
2018-02-17 21:04:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,7 +22,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5
   }
   do {
-    while array.length > 0 && maybeTrue()
+    while (array.length > 0 && maybeTrue())
   }
   return array
 }
